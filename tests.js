0bf7a4b628e3c1e6a6f7143c1c65988208ef27d5
Travis Horn <horn.travis@gmail.com>
2015-10-21 09:31:26 -0500

    Handle errors with arguments

--- tests.js
+++ tests.js
@@ -5,15 +5,78 @@
 var valivore = require('./index');
 
 describe('valivore', function() {
-  it('contains a validate function', function() {
-    valivore.validate();
+  describe('validate()', function() {
+    it('errors when no args specified', function() {
+      valivore.validate().should.be.an.Error();
+    });
+
+    it('errors when last arg isn\'t a function', function() {
+      valivore.validate({}, {}, 'test').should.be.an.Error();
+    });
+
+    it('errors when missing args', function() {
+      valivore.validate(function(err) {
+        err.should.be.type('string');
+      });
+
+      valivore.validate({}, function(err) {
+        err.should.be.type('string');
+      });
+    });
+
+    it('errors when missing args AND last arg isn\'t a function', function() {
+      valivore.validate({}).should.be.an.Error();
+      valivore.validate({}, {}).should.be.an.Error();
+    });
+
+    it('errors when wrong arg type', function() {
+      valivore.validate({}, 'test', function(err) {
+        err.should.be.type('string');
+      });
+
+      valivore.validate('test', {}, function(err) {
+        err.should.be.type('string');
+      });
+    });
+
+    it('doesn\'t give valErrs when missing arguments', function() {
+      valivore.validate(function(err, valErrs) {
+        (valErrs === null).should.be.true();
+      });
+
+      valivore.validate({}, function(err, valErrs) {
+        (valErrs === null).should.be.true();
+      });
+    });
+
+    it('doesn\'t give valErrs when incorrect arg type', function() {
+      valivore.validate({}, 'test', function(err, valErrs) {
+        (valErrs === null).should.be.true();
+      });
+
+      valivore.validate('test', {}, function(err, valErrs) {
+        (valErrs === null).should.be.true();
+      });
+    });
+
+    it('gives a valErr array', function() {
+      valivore.validate({}, {}, function(err, valErr) {
+        valErr.should.be.an.Array();
+      });
+    });
   });
 
-  describe('client-side', function() {
+  describe('cleanValidate()', function() {
+    it('doesn\'t give an error', function() {
+      valivore.cleanValidate({}, {}, function(err) {
+        (err === null).should.be.true();
+      });
+    });
 
-  });
-
-  describe('server-side', function() {
-
+    it('gives a valErr array', function() {
+      valivore.cleanValidate({}, {}, function(err, valErrs) {
+        valErrs.should.be.an.Array();
+      });
+    });
   });
 });
